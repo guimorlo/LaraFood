2801c59c484aef95bf4011c5dbe0f58ae3d8c456
guimorlo <guilhermehenriquemorlo@gmail.com>
2020-09-04 16:09:41 -0300

    larafood

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaraFood
                 </div>
 
                 <div class="links">
